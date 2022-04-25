e3f52dbecf036a923ac73354a4cfaf486023bdc4
Rashid <almaty-pro@yandex.ru>
2022-04-25 20:51:53 +0600

    на странице спасибо, тестовые методы для прикрепления оплаты

--- resources/views/thank-you.php
+++ resources/views/thank-you.php
@@ -10,6 +10,9 @@
 
 <?php
 
+use App\Models\Subscription;
+use Carbon\Carbon;
+
 $payments = \DB::table('pitech_notifications')
     ->orderBy('id', 'desc')
     ->get();
@@ -20,24 +23,43 @@ foreach ($payments as $payment){
     // print_r($payment->data);
 
     $result = json_decode($payment->data,true);
+    $subscription = Subscription::whereId($result[0]['extOrdersId'])->first();
 
-    foreach ($result as  $res){
-        echo $res['eventTime'];
-        echo "<br>";
-        echo $res['ordersTime'];
-        echo "<br>";
-        echo $res['extOrdersId'];
-        echo "<br>";
-        echo $res['extClientRef'];
-        echo "<br>";
-        echo $res['description'];
-        echo "<br>";
-        echo $res['ordersState'];
-        echo "<br>";
-        echo $res['authAmount'];
-        echo "<br><br><br>";
+    if ($subscription){
+        $payDate =  Carbon::parse($result[0]['ordersTime'])->setTimezone('Asia/Almaty');
+        $newEndedAt =  Carbon::parse($result[0]['ordersTime'])->setTimezone('Asia/Almaty')->addMonths(1);
+
+       Subscription::where('id', $result[0]['extOrdersId'])
+      //      ->where('id', $result[0]['extOrdersId'])
+            ->limit(1)
+           ->update(['status' => 'paid', 'ended_at' => $newEndedAt]);
+
+     //   print_r($subscription->data);
+        echo "<br><br>";
+        echo  $result[0]['extOrdersId'];
+        echo "<br><br>";
 
     }
+
+
+
+    //   print_r($result[0]);
+
+        echo $result[0]['eventTime'];
+        echo "<br>";
+        echo $result[0]['ordersTime'];
+        echo "<br>";
+        echo $result[0]['extOrdersId'];
+        echo "<br>";
+        echo $result[0]['extClientRef'];
+        echo "<br>";
+        echo $result[0]['description'];
+        echo "<br>";
+        echo $result[0]['ordersState'];
+        echo "<br>";
+        echo $result[0]['authAmount'];
+        echo "<br><br><br>";
+
 }
 
 ?>
