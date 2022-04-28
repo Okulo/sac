4355168be5eed5779970fcc5a30fc0d858091751
Rashid <almaty-pro@yandex.ru>
2022-04-28 14:34:54 +0600

    сохраняем оплату и считаем бонусы

--- resources/views/thank-you.php
+++ resources/views/thank-you.php
@@ -10,37 +10,97 @@
 
 <?php
 
+use App\Models\Payment;
+use App\Models\PaymentType;
+use App\Models\ProductBonus;
 use App\Models\Subscription;
 use Carbon\Carbon;
 
 $payments = \DB::table('pitech_notifications')
+    ->whereNull('deleted_at')
     ->orderBy('id', 'desc')
     ->get();
 
 
+foreach ($payments as $pay){
+   //  print_r($payment->id);
 
-foreach ($payments as $payment){
-    // print_r($payment->data);
+    $result = json_decode($pay->data,true);
+    //dd($result);
 
-    $result = json_decode($payment->data,true);
     $subscription = Subscription::whereId($result[0]['extOrdersId'])->first();
 
     if ($subscription){
+
         $payDate =  Carbon::parse($result[0]['ordersTime'])->setTimezone('Asia/Almaty');
         $newEndedAt =  Carbon::parse($result[0]['ordersTime'])->setTimezone('Asia/Almaty')->addMonths(1);
 
-       Subscription::where('id', $result[0]['extOrdersId'])
-      //      ->where('id', $result[0]['extOrdersId'])
+        $addSubscription = Subscription::where('id', $result[0]['extOrdersId'])
+            //      ->where('id', $result[0]['extOrdersId'])
             ->limit(1)
-           ->update(['status' => 'paid', 'ended_at' => $newEndedAt]);
+            ->update(['status' => 'paid', 'ended_at' => $newEndedAt]);
 
-     //   print_r($subscription->data);
+        //   print_r($subscription->data);
         echo "<br><br>";
         echo  $result[0]['extOrdersId'];
         echo "<br><br>";
+if($addSubscription){
 
+   $payment = Payment::updateOrCreate([
+            'transaction_id' => $result[0]['ordersId'],
+        ],
+        [
+            'subscription_id' => $subscription->id,
+            'user_id' => $subscription->user_id,
+            'product_id' => $subscription->product->id,
+            'customer_id' => $subscription->customer->id,
+            'quantity' => 1,
+            'type' => 'pitech',
+            'status' => 'Completed',
+            'amount' => $result[0]['totalAmount'],
+            'paided_at' => $result[0]['ordersTime'],
+            'team_id' => $subscription->team_id,
+            'data' =>  json_encode($result[0]),
+           //     'pitech' =>  $result,
+          //      'subscription' => $subscription,
+          // ],
+        ]);
+
+        // Присвоение бонуса к платежу
+        if (isset($payment->type) && isset($payment->subscription_id) && isset($payment->status) && $payment->status == 'Completed') {
+            $similarPaymentExists = $payment->subscription->payments()
+                ->where('status', 'Completed')
+                ->where('type', $payment->type)
+                ->where('paided_at', '<', $payment->paided_at)
+                ->where('id', '!=', $payment->id)
+                ->exists();
+            $paymentType = PaymentType::where('name', 'cloudpayments')->first();
+            if (! $paymentType) {
+                \Log::error('Отсутствует тип платежа. Payment ID: ' . $payment->id);
+            }
+            $type = $similarPaymentExists ? ProductBonus::REPEATED_PAYMENT : ProductBonus::FIRST_PAYMENT;
+
+
+            $bonus = ProductBonus::where('product_id', $payment->subscription->product_id)
+                ->where('payment_type_id', $paymentType->id)
+                ->where('type', $type)
+                ->active()
+                ->first();
+            if (! $bonus) {
+                \Log::error('Отсутствует бонус. Payment ID: ' . $payment->id);
+            } else {
+                $payment->product_bonus_id = $bonus->id;
+                echo "тут ".$pay->id."<br><br><br>";
+                \DB::table('pitech_notifications')
+                    ->where('id', $pay->id)
+                    ->update(['deleted_at' => "2022-04-27 01:01:01"]);
+            }
+        }
+        $payment->save();
+
+
+            }
     }
-
 
 
     //   print_r($result[0]);
