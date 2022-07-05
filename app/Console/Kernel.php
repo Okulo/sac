616f5a1c3a42e40634c2d94e36e7d2886eeada71
Rashid <almaty-pro@yandex.ru>
2022-07-05 20:24:52 +0600

    добавлен функционал привязки карты и триал периода

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('update:users_bonuses')->hourly();
         $schedule->command('update:pitech')->everyMinute();
         $schedule->command('pay:pitech')->dailyAt('01:00');
+        $schedule->command('trial:pitech')->dailyAt('01:00');
         $schedule->command('update:statistics week')->everySixHours();
         $schedule->command('update:statistics month')->dailyAt('13:00');
         $schedule->command('queue:restart')
--- app/Http/Controllers/Api/PitechController.php
+++ app/Http/Controllers/Api/PitechController.php
@@ -39,47 +39,47 @@ class pitechController extends Controller
     public function saveCardSuccess(Request $request)
     {
         try {
-            $data[] = $request->all();
-           // dd($data[0][0]);
-            if($data[0][0]['ordersState'] == 'SUCCESS') {
+            $data =  $request->all();
+
+            if($data['ordersState'] == 'SUCCESS') {
                 Card::updateOrCreate([
-                    'customer_id' => $data[0][0]['extClientRef'],
-                    'token' => $data[0][0]['cardsId'],
+                    'customer_id' => $data['extClientRef'],
+                    'token' => $data['cardsId'],
                 ],
                     [
-                        'customer_id' => $data[0][0]['extClientRef'],
+                        'customer_id' => $data['extClientRef'],
                         'cp_account_id' => 1,
                         'first_six' => "000000",
                         'last_four' => "0000",
                         'exp_date' => "00/00",
-                        'token' => $data[0][0]['cardsId'],
+                        'token' => $data['cardsId'],
                         'type' => 'pitech',
-                        'name' => $data[0][0]['card']['owner'] ?? '',
+                        'name' => $data['card']['owner'] ?? '',
 
                     ]);
 
                 $product = Subscription::leftJoin('products', 'subscriptions.product_id', '=', 'products.id')
-                   ->where('subscriptions.id', $data[0][0]['extOrdersId'] )
+                   ->where('subscriptions.id', $data['extOrdersId'] )
             ->where('subscriptions.status', 'trial')
             ->select('subscriptions.ended_at','products.*')
             ->orderBy('subscriptions.updated_at', 'desc')
             ->first();
 
               //  dd($product->ended_at);
-                $trial_date =  Carbon::parse($data[0][0]['eventTime'])->addDays($product->trial_period);
+                $trial_date =  Carbon::parse($data['eventTime'])->addDays($product->trial_period);
 
-                Subscription::where('id', $data[0][0]['extOrdersId'] )
+                Subscription::where('id', $data['extOrdersId'] )
                     ->where('status', 'trial')
-                    ->update(['started_at' => $data[0][0]['eventTime'], 'ended_at' => $trial_date]);
+                    ->update(['started_at' => $data['eventTime'], 'ended_at' => $trial_date]);
 
                 return response()->json([
                     'code' => 0,
                 ], 200);
             }
-            if ($data[0][0]['ordersState'] == 'ERROR'){
+            if ($data['ordersState'] == 'ERROR'){
 
                 UserLog::create([
-                    'subscription_id' => $data[0][0]['extOrdersId'],
+                    'subscription_id' => $data['extOrdersId'],
                     'user_id' => Auth::id(),
                     'type' => UserLog::SAVE_CARD_ERROR,
                     'data' => [
